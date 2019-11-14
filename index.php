3bd0f4389f922b17fc56fccfb2a10cc51b980eac
Yonel Ceruto <yonelceruto@gmail.com>
2019-11-14 18:56:19 -0500

    add vuejs to manage the UI

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Reproducer</title>
+    <script rel="prefetch" src="/assets/js/vue.min.js"></script>
+</head>
+<body>
 <?php
 
 session_start();
@@ -40,31 +51,55 @@ echo '</pre>';
 ?>
 <hr>
 <strong><?= exec('whoami') ?></strong>
-<form action="/" method="post">
-    <div>
-        <input id="cmd" name="cmd" autocomplete="off" autofocus="autofocus" width="200px" size="100" />
-        <button type="submit">exec</button>
-    </div>
-    <div>
-        <label><input type="checkbox" name="background"> Run in background</label>
-    </div>
-</form>
-
-<br>
-<strong>Last commands:</strong>
-<ul>
-    <?php foreach ($_SESSION['last_cmds'] as $i => $cmd) { ?>
-        <li>
-            <a href="#" id="cmd<?= $i ?>" onclick="document.getElementById('cmd').value = document.getElementById('cmd<?= $i ?>').innerHTML"><?= $cmd ?></a>
+<div id="app">
+    <form action="/" method="post">
+        <div>
+            <input id="cmd" name="cmd" autocomplete="off" autofocus="autofocus" width="200px" size="100" v-model="cmd"/>
+            <button type="submit">exec</button>
+        </div>
+        <div>
+            <label><input type="checkbox" name="background"> Run in background</label>
+        </div>
+    </form>
+    <br>
+    <strong>Last commands:</strong>
+    <ul id="last-cmds" data-value="<?= htmlentities(json_encode($_SESSION['last_cmds'])) ?>">
+        <li v-for="cmd in lastCmds">
+            <a href="javascript:" v-on:click="setCmd(cmd)">{{ cmd.value }}</a>
         </li>
-    <?php } ?>
-</ul>
-<strong>Utils:</strong>
-<ul>
-    <li>
-        <a href="#" onclick="document.getElementById('cmd').value = 'composer create-project '">composer create-project</a>
-    </li>
-    <li>
-        <a href="#" onclick="document.getElementById('cmd').value = '.apt/usr/lib/p7zip/7z '">7z</a>
-    </li>
-</ul>
+    </ul>
+    <strong>Utils:</strong>
+    <ul>
+        <li v-for="shortcut in shortcuts">
+            <a href="javascript:" v-on:click="setCmd(shortcut)">{{ shortcut.value }}</a>
+        </li>
+    </ul>
+</div>
+
+<script>
+    let app = new Vue({
+        el: '#app',
+        data: {
+            cmd: '',
+            lastCmds: [],
+            shortcuts: [
+                {'value': 'composer create-project symfony/skeleton '},
+                {'value': '.apt/usr/lib/p7zip/7z '},
+                {'value': 'ps -p '},
+            ],
+        },
+        methods: {
+            setCmd: function (shortcut) {
+                this.cmd = shortcut.value;
+                document.getElementById('cmd').focus();
+            }
+        },
+        created() {
+            const cmds = JSON.parse(document.getElementById('last-cmds').dataset.value);
+            this.lastCmds = cmds.map(c => ({value: c}));
+        }
+    })
+</script>
+
+</body>
+</html>
