ea6fdeb1045c5e464df2c288cb5bb69d79a7c845
Yonel Ceruto <yonelceruto@gmail.com>
2019-11-15 06:56:24 -0500

    fix assets path

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Reproducer</title>
-    <script rel="prefetch" src="/public/assets/js/vue.min.js"></script>
+    <script rel="prefetch" src="/assets/js/vue.min.js"></script>
 </head>
 <body>
 <?php
